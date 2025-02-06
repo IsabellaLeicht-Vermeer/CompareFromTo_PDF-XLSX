b6d76740edeef7cb2ca05cd7ec40b17f05606b8e
Isabella Leicht <isabellaleicht@vermeer.com>
2025-02-06 14:42:13 -0600

    cleaned up main

--- main.py
+++ main.py
@@ -3,32 +3,12 @@ import lib.functions
 if __name__ == "__main__":
     dirFile = input("Input file directory including filename (no file extension): ")
     pdfTables = (lib.functions.GetTables(docDirectory = dirFile + ".pdf"))
-    #for i in pdfTables:
-    #    print(i)
-    #print(len(pdfTables))
-    #print("")
-
     excelTables = lib.functions.GetExcel(docDirectory = dirFile + ".xlsx")
-    #for i in excelTables:
-    #    print(i)
-    #print(len(excelTables))
-
     parsedExcel = sorted(lib.functions.ParseExcel(excelTables), key=lambda d: d['signal'])
-    #for i in parsedExcel:
-    #    print(i)
-    #print(len(parsedExcel))
-    #print("")
-    
     parsedPDF = sorted(lib.functions.ParsePDFTables(pdfTables), key=lambda d: d['signal'])
-    #for i in parsedPDF:
-    #    print(i)
-    #print(len(parsedPDF))
-    #print("")
-
     outputCompare = lib.functions.CompareFromTo(parsedExcel, parsedPDF)
-    #lib.functions.CompareFromTo(parsedExcel, parsedPDF)
     
-    #print("")
+
     if len(outputCompare[0]) > 0:
         print("Unaccounted for from Excel: ", end="\n")
         for i in (outputCompare[0]):
